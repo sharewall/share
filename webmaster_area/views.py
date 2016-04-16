9a54ea7eafd0301017247efc68a549bb5eea058d
freemanIT <freemanit1512@gmail.com>
2016-04-16 14:46:51 +0300

    6

--- webmaster_area/views.py
+++ webmaster_area/views.py
@@ -598,7 +598,7 @@ def checkconfig(request):
             answer += 'console.log("url ok");'
             #TODO: snc from server
             answer += 'console.log("snc from client: '+snc+'");'
-            snc = setcounterprivate(request_url)
+            snc = setcounterprivate(url=request_url)
             answer += 'console.log("snc from server: '+snc+'");'
             
             #wma today!
